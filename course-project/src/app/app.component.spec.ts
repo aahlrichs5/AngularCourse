be59e514dfd04fd41d003aa2b2c67a045681b37d
Alex Ahlrichs <ahlrichsa@cartegraph.com>
2021-05-25 16:27:43 -0500

    course project running now

--- course-project/src/app/app.component.spec.ts
+++ course-project/src/app/app.component.spec.ts
@@ -4,7 +4,9 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AppComponent],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,18 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'course-project'`, () => {
+  it(`should have as title 'my-first-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    //expect(app.title).toEqual('course-project');
+    //expect(app.title).toEqual('my-first-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain(
-      'course-project app is running!'
-    );
+    expect(compiled.querySelector('.content span').textContent).toContain('my-first-app app is running!');
   });
 });
