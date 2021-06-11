ec9540cee055027adc55fba31c7374d6f46293ba
Alex Ahlrichs <ahlrichsa@cartegraph.com>
2021-06-11 15:20:55 -0500

    create files for course project, add items to some files

---
+++ course-project/src/app/Header/RecipeBook/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is a test',
+      'https://cdn.pixabay.com/photo/2020/10/21/19/17/soup-5674071_1280.jpg'
+    ),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -3,9 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header/header.component';
+import { RecipeDetailComponent } from './Header/RecipeBook/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './Header/RecipeBook/recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './Header/RecipeBook/recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './Header/RecipeBook/recipes/recipes.component';
+import { IngredientComponent } from './Header/ShoppingList/ingredient/ingredient.component';
+import { ShoppingEditComponent } from './Header/ShoppingList/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './Header/ShoppingList/shopping-list/shopping-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    IngredientComponent,
+    ShoppingEditComponent,
+    ShoppingListComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
