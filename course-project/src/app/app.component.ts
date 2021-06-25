d5ff111b0eae7c48bd93d8d5b660f0317b2ebab0
Alex Ahlrichs <ahlrichsa@cartegraph.com>
2021-06-25 15:30:34 -0500

    Recipe Updates

--- course-project/src/app/app.component.ts
+++ course-project/src/app/app.component.ts
@@ -3,4 +3,10 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
+}
