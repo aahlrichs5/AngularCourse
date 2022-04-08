4a73f4dfc2da862abe2d564a0a0fe504c381a266
Alex Ahlrichs <aahlrichs5@gmail.com>
2022-04-08 16:07:58 -0500

    Remove event emitters for subscriptions

--- course-project/src/app/Header/RecipeBook/recipes/recipes.component.ts
+++ course-project/src/app/Header/RecipeBook/recipes/recipes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { RecipeService } from '../recipe.service';
-import { Recipe } from './recipes.model';
 
+import { RecipeService } from '../recipe.service';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
@@ -9,13 +8,7 @@ import { Recipe } from './recipes.model';
   providers: [RecipeService],
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
+  constructor() {}
 
-  constructor(private recipeService: RecipeService) {}
-
-  ngOnInit(): void {
-    this.recipeService.recipeSelected.subscribe((recipe: Recipe) => {
-      this.selectedRecipe = recipe;
-    });
-  }
+  ngOnInit(): void {}
 }
